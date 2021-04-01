2e967dfc823d46bc4adadab26bf2371b13589191
tarourskyy <pchytara@gmail.com>
2021-04-01 19:45:04 +0700

    menambahkan project

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int num1, int num2)
+        {
+            if (num1 >= num2) 
+                return num2;
+            else
+                return num1;
+        }
+
+        public int FindMinimum(int num1, int num2, float num3)
+        {
+            if (num1 >= num2 || num2 >= num3)
+            {
+                if (num2 < num1 && num2 < num3)
+                    return num2;
+                else
+                    return (int)num3;
+            }
+            else return num1;
+        }
+
+        public int FindMaximum(int num1, int num2)
+        {
+            if (num1 <= num2)
+                return num2;
+            else
+                return num1;
+        }
+
+        public int FindMaximum(int num1, int num2, float num3)
+        {
+            if (num1 <= num2 || num1 <= num3)
+            {
+                if (num2 > num1 && num2 > num3)
+                    return num2;
+                else
+                    return (int)num3;
+            }
+            else return num1;
+        }
+    }
+}
---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers num = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", num.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", num.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", num.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", num.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
